d69a1e5fc40767456b895d28a6d49b65dfa97e55
wangxiaolong <lonng1993>
2019-12-10 13:11:16 +0800

    最终版吸血鬼数字

--- src/test/java/com/zeroten/flow/javeTest.java
+++ src/test/java/com/zeroten/flow/javeTest.java
@@ -9,10 +9,10 @@ import java.util.Arrays;
 public class javeTest {
 
     @Test
-    //吸血鬼练习题
-    public void test3() {
+    //吸血鬼
+    public void javaTest3() {
 
-        int[] arryTotal=new int[0];
+        String[] arryTotal=new String[0];
         for (int i=1000;i<=9999;i++){
             //千位上的数字
             int a=i/1000;
@@ -28,31 +28,49 @@ public class javeTest {
             arry[2]=c;
             arry[3]=d;
            // System.out.println(Arrays.toString(arry));
-            for(int j=0;j<arry.length;j++){
+            for(int j=0;j<arry.length-1;j++){
                 for (int k=j+1;k<arry.length;k++){
                     String s1=String.valueOf(arry[j])+String.valueOf(arry[k]);
-                    String s2=String.valueOf(arry[arry.length-1-j])+String.valueOf(arry[arry.length-1-k]);
+                    String s2="";
+                    if( k<arry.length-1){
+                        s2=String.valueOf(arry[arry.length-1-j])+String.valueOf(arry[arry.length-1-k]);
+                    }
+                    if(j==0 && k==arry.length-1){
+                        s2=String.valueOf(arry[k-1])+String.valueOf(arry[j+1]);
+                    }
+                    if(j!=0 && k==arry.length-1){
+                        s2=String.valueOf(arry[k-j])+String.valueOf(arry[arry.length-1-k]);
+                    }
                     int l1=Integer.valueOf(s1);
                     int l2=Integer.valueOf(s2);
                     if(l1*l2==i) {
-                        //判断吸血鬼数字是否已经存在
+                       //判断吸血鬼数字是否已经存在
                         int num=0;
                         for(int n=0;n<arryTotal.length;n++){
-                            if (i==arryTotal[n]){
+                            if (arryTotal[n].contains(String.valueOf(i))){
                                num++;
                             }
                         }
                         //不存在继续添加吸血鬼数字
                         if(num==0){
                             arryTotal=Arrays.copyOf(arryTotal,arryTotal.length+1);
-                            arryTotal[arryTotal.length-1]=i;
+                            arryTotal[arryTotal.length-1]=String.valueOf(i)+"="+s1+"*"+s2;
                         }
                     }
                 }
+
             }
         }
         System.out.println("吸血鬼数字应该有" + arryTotal.length+"个");
         System.out.println("吸血鬼数字是" + Arrays.toString(arryTotal));
+    }
+
+    @Test
+    //回文数
+    public void javaTest4(){
+
+
+
     }
 
 
